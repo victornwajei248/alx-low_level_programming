3f6b0ead3ae3642070f061d990286900598e1ec6
victor <victornwajei248@gmail.com>
2022-10-17 15:56:15 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,10 @@
-nclude <stdio.h>
+#include <stdio.h>
 /**
+ *main- Serves as the entry point for the program
  *
- *  *main- Serves as the entry point for the program
- *
- *   *
- *
- *    *Return: By default returns zero if no erros otherwise returns non-zero value.
- *
- *     */
+ *Return: By default returns zero if no erros otherwise returns non-zero value
+ */
 int main(void)
 {
-		puts("\"Programming is like building a multilingual puzzle");
-
-			return (0);
+	puts("\"Programming is like building a multilingual puzzle");return (0);
 }
