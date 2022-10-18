1be4c3c2d3e48bd4689d08d5f58998aec9432619
victor <victornwajei248@gmail.com>
2022-10-17 19:45:37 -0700

    task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,10 +2,10 @@
 #include <time.h>
 #include <stdio.h>
 /**
-*main-program entry point.
-*
-*Return:0 if no error, non-zero value if error.
-*/
+ *main-program entry point.
+ *
+ *Return:0 if no error, non-zero value if error.
+ */
 int main(void)
 {
 	int n;
