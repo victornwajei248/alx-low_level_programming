a415f4762a5a73133c7ec524ec2ebc21f64d9ac7
victor <victornwajei248@gmail.com>
2022-10-19 11:34:13 -0700

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ **main - Entry point
+ **Description: working with standard library function putchar
+ **Return: 0
+ **/
+
+int main(void)
+{
+	putchar('D');
+	return 0;
+}
