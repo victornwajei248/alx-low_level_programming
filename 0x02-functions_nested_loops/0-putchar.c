a67623be2b9ba3bac92b6dff1efb0f555397c7b8
victor <victornwajei248@gmail.com>
2022-10-20 07:58:55 -0700

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- *  _isupper - checks if the letters are uppercase
- *  @c: input.
+ * _isupper - checks if the letters are uppercase
+ * @c: input.
  *
- *  Return: 1 if c is uppercase
- *  0 otherwise.
+ * Return: 1 if c is uppercase
+ * 0 otherwise.
  */
 int _isupper(int c)
 {
